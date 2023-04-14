8a6ddb4393d4a24933b08f5865646e0d0ac5a502
FabioReact <fabio.react.dev@gmail.com>
2023-04-14 09:47:11 +0200

    test: add success and missing required param test

---
+++ src/tests/api/user.test.ts
@@ -0,0 +1,45 @@
+import { User, registerUser } from "@/api/user";
+// Mettre en place un mock de notre API grâce à msw
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+	rest.post("http://localhost:4000/register", (req, res, ctx) => {
+		return res(ctx.json({ accessToken: "xxx.xxx.xxx" }));
+	}),
+);
+
+beforeAll(() => {
+	server.listen();
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => {
+	server.close();
+});
+
+describe("user requests", () => {
+	describe("registerUser", () => {
+		it("should a valid accessToken", async () => {
+			const response = (await registerUser({
+				email: "valid@email.com",
+				password: "validPassword",
+			})) as { accessToken: string };
+			expect(response.accessToken).toBe("xxx.xxx.xxx");
+		});
+	
+		it("should return missing param error", async () => {
+			server.use(
+				rest.post("http://localhost:4000/register", (req, res, ctx) => {
+					return res(
+						ctx.status(400),
+						ctx.json("Email and password are required"),
+					);
+				}),
+			);
+			const response = (await registerUser({
+				password: "",
+			} as User)) as { error: string };
+			expect(response.error).toBe("Email and password are required");
+		});
+	});
+});
