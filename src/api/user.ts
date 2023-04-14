5aee366c467d86b8805e11acca8e57ff8577e36d
FabioReact <fabio.react.dev@gmail.com>
2023-04-14 09:46:45 +0200

    feat: improve catch error message in registerUser

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,31 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
-type User = {
-	email: string
-	firstname?: string
-	lastname?: string
-	photo?: string
-	password: string
-}
+export type User = {
+	email: string;
+	firstname?: string;
+	lastname?: string;
+	photo?: string;
+	password: string;
+};
 
 const fetcher = {
 	post<T>(url: string, data: unknown) {
-		return axios.post<T>(url, data)
+		return axios.post<T>(url, data);
 	},
-	get() {}
-}
+	get() {},
+};
 
 export const registerUser = (user: User) => {
-	return fetcher.post<{ accessToken: string }>('http://localhost:4000/register', user).catch(e => {
-		return null
-	})
-}
+	return fetcher
+		.post<{ accessToken: string }>("http://localhost:4000/register", user)
+		.then((response) => {
+			return response.data;
+		})
+		.catch((err) => {
+			const error: string = err.response?.data as string || 'Server Error'
+			return {
+				error
+			};
+		});
+};
 
-export const loginUser = () => {
-	
-}
+export const loginUser = () => {};
 
-export const fetchAllUsers = () => {
-	
-}
-
+export const fetchAllUsers = () => {};
